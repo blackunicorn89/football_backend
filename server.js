078bb3107c51b97ae2f893a680bef536304d70a7
Timo Katila <timo@tkatila.com>
2022-10-07 11:57:11 +0300

    Aurthorization middleware for JWT

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const usersRoutes = require("./routes/users-routes");
 const app = express()
 app.use(bodyParser.json()) //Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
+// Cors Policy
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET", "POST", "PATCH", "DELETE");
+  next();
+});
 
 app.use("/api/users", usersRoutes); //Users Sign up, Login routes.
 
