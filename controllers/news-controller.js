2ac7c9463a84d01a49dfafcaeb7be8aee40176d7
Antti Helander <hela_ant@outlook.com>
2022-10-07 12:52:27 +0300

    checkauth ei toimi news-routessa

--- controllers/news-controller.js
+++ controllers/news-controller.js
@@ -13,7 +13,7 @@ const getNews = async (req, res, next) => {
     let newsArticles;
 
     try {
-       newsArticles  = await news.find()
+       newsArticles  = await news.find({})
     
       } catch (err) {
         const error = new HttpError("Failed to find articles. Reason",
@@ -22,10 +22,35 @@ const getNews = async (req, res, next) => {
         return next(error);
         
       }
-      res.json({ newsArticles: newsArticles.map(article=> article.toObject({ getters: true })) });
-     
+      res.json({ newsArticles: newsArticles.map(article => article.toObject({ getters: true })) });
+     }
+
+let addNewsArticle = async(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
+  }
+  const { header, content, date } = req.body
+
+  const createdNewsArticle = new news({
+    header,
+    content,
+    date,
+  });
+
+  try {
+    await createdNewsArticle.save();
+  } catch (err) {
+    const error = new HttpError("Creating news article failed, please try again", 500)
+    return next(error);
+  }
+
+  res.status(201).json({Message: "Succesfully created" });  
 
 
 }
 
 exports.getNews = getNews;
+exports.addNewsArticle = addNewsArticle;
--- routes/news-route.js
+++ routes/news-route.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const { check } = require("express-validator")
 const newsController = require("../controllers/news-controller")
+const checkAuth = require("../middleware/check-auth")
 
 
 router = express.Router();
 
 router.get("/news", newsController.getNews);
+
+router.use(checkAuth);
+router.post("/news",
+  [
+    check("header").not().isEmpty(),
+    check("content").not().isEmpty(),
+    check("date").not().isEmpty(),
+  ],
+  newsController.addNewsArticle);
 
 module.exports = router;
