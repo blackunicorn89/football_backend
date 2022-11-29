ac8ff60f8e14a3ba6846e9ceda3c3fef2ede06cd
Antti Helander <hela_ant@outlook.com>
2022-11-29 15:06:08 +0200

    Alettu luomaan kauden pelien näyttöä uusiksi

--- models/season.js
+++ models/season.js
@@ -8,6 +8,14 @@ const seasonSchema = new Schema({
   active: Boolean
 });
 
+const gameOfSeasonSchema = Schema({
+  _id: Schema.Types.ObjectId,
+  season_name: {type: String, unique: true},
+  active: boolean,
+  season_games: [{ type: Schema.Types.ObjectId, ref: 'seasongames' }]
+});
+
 seasonSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Season", seasonSchema);
+
