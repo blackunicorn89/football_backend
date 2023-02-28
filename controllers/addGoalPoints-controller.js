9adca104d86d175532c384ea7c82e75772e8ba88
Antti Helander <hela_ant@outlook.com>
2023-02-28 14:44:15 +0200

    pisteiden lisäys pelaajalle toimii, kun json-muotoisen datan mukana tulee taulu, jossa on json-muodossa yhden pelaajan tiedot. Seuraavaks testataan monen pelaajan tiedoilla

--- controllers/addGoalPoints-controller.js
+++ controllers/addGoalPoints-controller.js
@@ -2,7 +2,7 @@ const { validationResult } = require("express-validator");
 const fs = require("fs");
 const HttpError = require("../models/http-error");
 const MySqlDb = require("../models");
-const MySqlGameModel = MySqlDb.Game;
+const Player = MySqlDb.Player;
 
 
 // LISTAA KAIKKI KAUDEN PELIT
@@ -55,32 +55,65 @@ const addGoalPoints = async (req, res, next) => {
   }
 
   
-  //const goal_makers = [{"name": "Testi pelaaja 1", "points": "3", "id": "1"}, {"name": "Testi pelaaja 2", "points": "1", "id": "2"}, {"name": "Testi pelaaja 3", "points": "5", "id": "3"} ]
+  const static_goal_makers = [{"name": "Testi pelaaja 1", "points": "3", "id": "1"}, {"name": "Testi pelaaja 2", "points": "1", "id": "2"}, {"name": "Testi pelaaja 3", "points": "5", "id": "3"} ] 
   
-  //console.log("Tyyppi alustuksessa: " + typeof goal_makers)
   const goal_makers = req.body
-  points = goal_makers
-  let goalMakerId = 0;
-
+  //const points = goal_makers.goal_makers
+  
 
   try {
 
-    console.log(goal_makers)
-    for (const key in goal_makers) {
-      console.log(key, data[key]);
-    }
     
-    console.log(goalMakerId)
+    
+    for (i = 0; i < goal_makers.goal_makers.length; i ++) {
+
+      let currentGoalMakerPoints = 0;
+      
+      let goalMakerId = 0;
+      let newGoalMakerPoints = 0;
+      //Tietokannan sarakkeen arvo, jonka arvo tullaan päivittämään
+      let goal_points = 0 
+      let goalMaker;
+
+
+      goalMakerId = parseInt(goal_makers.goal_makers[i].id)
+      newGoalMakerPoints = parseInt(goal_makers.goal_makers[i].points)
+      //console.log("Pelaajan id: " + points[i].id + " pelajaan nimi: "+ points[i].id + " pelaajan pisteet " + points[i].points)
+
+      //console.log("pelkkä id: " + goalMakerId + " ja sen tyyppi: " + typeof goalMakerId)
+      //console.log("pelkät pisteet: " + goalMakerPoints + " ja niiden tyyppi: " + typeof goalMakerPoints)
+      
+      goalMaker = await Player.findByPk(goalMakerId, {
+        attributes: ["player_name", "id", "goal_points"]
+      })
+
+
+
+      currentGoalMakerPoints = goalMaker.goal_points
+      console.log("Nykyiset pisteet ovat " + currentGoalMakerPoints)
+      goal_points = currentGoalMakerPoints + newGoalMakerPoints
+      console.log("nykyiset pisteet lisäyksen jälkeen: " + goal_points)
+      
+      const addGoalPoints = {
+        goal_points
+      } 
+
+      Player.update(addGoalPoints, {where:{id: goalMakerId}})
+    
+
+    }
+   
+    
     //await MySqlGameModel.create(newGame);
   } catch (err) {
     const error = new HttpError(
-      "Adding new game failed, try again later",
+      "Adding points failed, try again later",
       500
     );
     return next(error);
   }
 
-  res.status(201).json("jee");
+  res.status(201).json("onnstui");
 
 };
 
